22e3d0496fcfadfa8fe1a81caccb81174216c085
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2016-07-15 14:37:12 +0530

    The 1st Commit

---
+++ SIMPLE.c
@@ -0,0 +1,10 @@
+#include<iostream.h>
+#include<conio.h>
+#include<math.h>
+
+void main()
+{
+cout<<"This is where the self written SIMPLE solver will be updated"<<endl;
+cout<<"Please stay tuned for more details";
+}
+
